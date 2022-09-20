91fadb6f73a571109107a68aaae24d608aa971e3
Lcourse <101373417+Lcourse@users.noreply.github.com>
2022-09-19 23:39:57 -0300

    Update pixabay.js

--- 02-pixabay/pixabay.js
+++ 02-pixabay/pixabay.js
@@ -7,22 +7,55 @@ const searchImages = async (text) => {
   return response.json();
 };
 
-const createCard = ({ webformatURL }) => {
+const createLink = (tag) => `
+      <a href="#" onClick="loadGallery('${tag}')">
+        ${tag}
+      </a>
+
+`;
+
+const createCard = ({ webformatURL, pageURL, tags, likes, comments }) => {
   const card = document.createElement("div");
   card.classList.add("card-container");
   card.innerHTML = `
-  <a href="#" class="card-image">
+  <a href="${pageURL}" class="card-image">
     <img src=${webformatURL} >
+    </a>
+    <div class="card-info">
+        <div class="card-tags">
+           ${tags.split(",").map(createLink).join("")}  
+        </div>
+        <div class="card-action">
+              <div class="card-like">
+                <i class="far fa-thumbs-up"></i>
+                <span>${likes}</span>
+              </div>
+              <div class="card-comment">
+              <i class="far fa-comment"></i>
+              <span>${comments}</span>
+              </div>
+              <div class="card-save">
+              <i class="far fa-bookmark"></i>
+              
+              </div>
+        </div>
+
+
+    </div>
   `;
   return card;
 };
 
-const loadGallery = async (text) => {
+const loadGallery = async (text, page = 1) => {
   const container = document.querySelector(".container-gallery");
-  const { hits } = await searchImages(text);
+  const { hits, totalHits } = await searchImages(`${text}&page=${page}`);
   const cards = hits.map(createCard);
   container.replaceChildren(...cards);
-  console.log(cards);
+
+  const totalPages = Math.ceil(totalHits / 20);
+  document.querySelector("#page-total").textContent = `/ ${totalPages}`;
+  document.querySelector("#search-input").value = text;
+  document.querySelector("#page").value = page;
 };
 
 const handleKeypress = ({ key, target }) => {
@@ -31,8 +64,39 @@ const handleKeypress = ({ key, target }) => {
   }
 };
 
+const handlePage = ({ key, target }) => {
+  const text = document.querySelector("#search-input").value;
+  if (key === "Enter") {
+    loadGallery(text, target.value);
+  }
+};
+
+const handleNext = () => {
+  let page = Number(document.querySelector("#page").value);
+  const totalPages = Number(
+    document.querySelector("#page-total").textContent.replace("/", "")
+  );
+  const text = document.querySelector("#search-input").value;
+  if (page < totalPages) {
+    page++;
+    loadGallery(text, page);
+  }
+};
+
+const handlePrevious = () => {
+  let page = Number(document.querySelector("#page").value);
+  const text = document.querySelector("#search-input").value;
+  if (page > 1) {
+    page--;
+    loadGallery(text, page);
+  }
+};
+
 document
   .querySelector("#search-input")
   .addEventListener("keypress", handleKeypress);
-
-
+document.querySelector("#page").addEventListener("keypress", handlePage);
+document.querySelector("#page-next").addEventListener("click", handleNext);
+document
+  .querySelector("#page-previous")
+  .addEventListener("click", handlePrevious);
